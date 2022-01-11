8e021245c03eb651d0caa390932c31737b0026ef
JerryWangCR <jerry.cr.w@gmail.com>
2022-01-11 01:05:20 -0500

    OMG Cralwer worked

--- covid_19_scrap/covid_19_scrap/spiders/covid_spider.py
+++ covid_19_scrap/covid_19_scrap/spiders/covid_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy import Request
 from scrapy.linkextractors import LinkExtractor
 
 class CovidSpider(scrapy.Spider):
@@ -8,22 +9,29 @@ class CovidSpider(scrapy.Spider):
     start_urls = [
         'https://www.worldometers.info/coronavirus/#countries'
     ]
-    allowed_domains=['https://www.worldometers.info/coronavirus/']
-    def __init__(self):
-        self.link_extractor = LinkExtractor(allow="https://www.worldometers.info/coronavirus")
-    
+    allowed_domains=['www.worldometers.info']
+   
     def parse(self, response):
-        for links in self.link_extractor.extract_links(response):
-            with open('covid_links.json', 'a+') as f:
-                f.write(f"\n{str(link)}")
-
-            yield response.follow(url=links, callback=self.parse)
-            # yield {
-            #     'Country': response.css('.label-counter::text')[-1].get().split('\n')[-1],
-            #     'TotalCases': response.css('.maincounter-number >span::text')[0].get(),
-            #     'TotalDeath': response.css('.maincounter-number >span::text').extract()[1],
-            #     'TotalRecovered': response.css('.maincounter-number >span::text').extract()[2]
+        for links in response.css('tbody > tr > td > a.mt_a::attr(href)'):
+            #newLink = "https://www.worldometers.info/coronavirus/" + links.get()
+            # yield{
+            #     'link' : newLink
             # }
+            yield response.follow("https://www.worldometers.info/coronavirus/" + links.get(), callback=self.parse_link)
+            # with open('covid_links.json', 'a+') as f:
+            #     f.write(f"\n{str(links)}")
+
+        #     links = response.follow(url=links, callback=self.parse)
+        # print(links[27:250])
+
+    def parse_link(self, response):
+
+             yield {
+                 'Country': response.css('.label-counter::text')[-1].get().split('\n')[-1],
+                  'TotalCases': response.css('.maincounter-number >span::text')[0].get(),
+                  'TotalDeath': response.css('.maincounter-number >span::text').extract()[1],
+                  'TotalRecovered': response.css('.maincounter-number >span::text').extract()[2]
+              }
     
 
         # next_page = response.css('a.mt_a::attr(href)').get()
